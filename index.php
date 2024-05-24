8e1bf50e43e3589679298bbfe6ae1e4e5568a0d4
woxy <tortles1226@gmail.com>
2024-05-24 11:45:10 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URSM FACILITIES E-MONITORING AND SCHEDULING SYSTEM</title>
+</head>
+<body>
+    <p>WELCOME USERS!</p>
+</body>
+</html>
